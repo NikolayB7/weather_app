f5656a80e729b0d1757b075eb4b74a61db2baa3c
NikolayB7 <biletskiyna@gmail.com>
2023-12-21 13:08:40 +0200

    map style

--- src/Components/Map.jsx
+++ src/Components/Map.jsx
@@ -6,8 +6,8 @@ const Map = () => {
     const position = [51.505, -0.09]
     return (
         <div className="map-container">
-            <MapContainer center={[51.505, -0.09]} zoom={13} style={{ height: '100vh' }}>
-                <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            <MapContainer center={[51.505, -0.09]} zoom={10} style={{ height: '100vh' }}>
+                <TileLayer url="https://tiles.stadiamaps.com/tiles/stamen_toner_lite/{z}/{x}/{y}{r}.png" />
                 <Marker position={[51.505, -0.09]}></Marker>
             </MapContainer>
         </div>
